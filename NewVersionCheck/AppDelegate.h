3e453951577badd626c532165d59b9157947da4a
SudhaPatilI <spiosdeveloper@gmail.com>
2015-11-04 10:59:56 +0530

    Initial Commit

---
+++ NewVersionCheck/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewVersionCheck
+//
+//  Created by Manjit on 04/11/15.
+//  Copyright (c) 2015 Manjit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewVersionCheck/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewVersionCheck
+//
+//  Created by Manjit on 04/11/15.
+//  Copyright (c) 2015 Manjit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
